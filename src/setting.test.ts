47b3c45c1815dafd3b2e7ddd4f216483f8e529b1
Akiomi Kamakura <akiomik@gmail.com>
2019-12-11 02:18:00 +0900

    Improve tests for setting

--- src/setting.test.ts
+++ src/setting.test.ts
@@ -4,16 +4,39 @@ import { Setting } from './setting'
 declare const global: any
 
 test('load', () => {
-  // TODO: Use jest mock
+  const mockGetProperty = jest.fn()
+  mockGetProperty.mockReturnValue('foo')
+
   global.PropertiesService = {
     getUserProperties: (): object => {
       return {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        getProperty: (): any => 'fuga'
+        getProperty: mockGetProperty
       }
     }
   }
 
   const setting = Setting.load()
-  expect(setting.token).toBe('fuga')
+  expect(mockGetProperty.mock.calls.length).toBe(1)
+  expect(setting.token).toBe('foo')
+})
+
+test('save', () => {
+  const mockGetProperty = jest.fn()
+  const mockSetProperty = jest.fn()
+
+  global.PropertiesService = {
+    getUserProperties: (): object => {
+      return {
+        getProperty: mockGetProperty,
+        setProperty: mockSetProperty
+      }
+    }
+  }
+
+  const setting = Setting.load()
+  setting.token = 'bar'
+  setting.save()
+
+  expect(mockSetProperty.mock.calls.length).toBe(1)
+  expect(mockSetProperty.mock.calls[0]).toEqual([Setting.tokenProperty, 'bar'])
 })
