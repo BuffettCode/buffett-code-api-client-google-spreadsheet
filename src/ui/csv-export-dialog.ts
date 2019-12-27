5847a98ff1f068b8ba7d5b8962c814b1e970e3b6
Akiomi Kamakura <akiomik@gmail.com>
2019-12-27 18:19:19 +0900

    Improve export-csv-dialog

--- src/ui/csv-export-dialog.ts
+++ src/ui/csv-export-dialog.ts
@@ -1,11 +1,5 @@
 import { CsvExporter } from '../services/csv-exporter'
 
 export function exportCsv(ticker: string, from: string, to: string): void {
-  try {
-    CsvExporter.exportCsv(ticker, from, to)
-  } catch (e) {
-    Logger.log(e.message)
-    const ui = SpreadsheetApp.getUi()
-    ui.alert('エラーが発生しました', e.message, ui.ButtonSet.OK)
-  }
+  CsvExporter.exportCsv(ticker, from, to)
 }
