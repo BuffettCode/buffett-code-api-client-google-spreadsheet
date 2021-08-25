36d847f7b7698958cdc5f5cd6e70fe659f148f04
Akiomi Kamakura <akiomik@gmail.com>
2021-08-25 13:42:59 +0900

    Add v3 api client

---
+++ src/api/v3/client.ts
@@ -0,0 +1,112 @@
+import { HttpError } from '~/api/http-error'
+import { UrlBuilder } from '~/api/url-builder'
+import { YearQuarterParam } from '~/fiscal-periods/year-quarter-param'
+
+export class BuffettCodeApiClientV3 {
+  static readonly baseUrl = 'https://api.buffett-code.com/api/v3'
+
+  constructor(private _token: string) {}
+
+  private static request(url: string, options = {}): object {
+    const defaultOptions = {
+      muteHttpExceptions: true
+    }
+    const fullOptions = { ...defaultOptions, ...options }
+    const res = UrlFetchApp.fetch(url, fullOptions)
+
+    const code = res.getResponseCode()
+    const content = res.getContentText()
+    const error = new HttpError(res)
+    if (
+      Math.floor(code / 100) === 4 ||
+      Math.floor(code / 100) === 5 ||
+      error.isInvalidTestingRequest()
+    ) {
+      throw error
+    }
+
+    let json
+    try {
+      json = JSON.parse(content)
+    } catch (e) {
+      console.error('JSON parsing error', code, content)
+      throw new HttpError(res)
+    }
+
+    return json
+  }
+
+  private defaultOptions(): object {
+    return {
+      headers: {
+        'x-api-key': this._token
+      }
+    }
+  }
+
+  public company(ticker: string): object {
+    const endpoint = BuffettCodeApiClientV3.baseUrl + '/company'
+    const builder = new UrlBuilder(endpoint, { ticker: ticker })
+    const url = builder.toString()
+    const options = this.defaultOptions()
+
+    const res = BuffettCodeApiClientV3.request(url, options)
+    return res[ticker]
+  }
+
+  public quarter(ticker: string, period: YearQuarterParam): object {
+    const endpoint = BuffettCodeApiClientV3.baseUrl + '/quarter'
+    const builder = new UrlBuilder(endpoint, {
+      ticker,
+      fy: period.year,
+      fq: period.quarter
+    })
+    const url = builder.toString()
+    const options = this.defaultOptions()
+
+    const res = BuffettCodeApiClientV3.request(url, options)
+    return res['data']
+  }
+
+  public ondemandQuarter(ticker: string, period: YearQuarterParam): object {
+    const endpoint = BuffettCodeApiClientV3.baseUrl + '/ondemand/quarter'
+    const builder = new UrlBuilder(endpoint, {
+      ticker,
+      fy: period.year,
+      fq: period.quarter
+    })
+    const url = builder.toString()
+    const options = this.defaultOptions()
+
+    const res = BuffettCodeApiClientV3.request(url, options)
+    return res['data']
+  }
+
+  // TODO: latestのサポート
+  public daily(ticker: string, date: Date): object {
+    const endpoint = BuffettCodeApiClientV3.baseUrl + '/daily'
+    const builder = new UrlBuilder(endpoint, {
+      ticker,
+      date: date.toISOString().substring(0, 10)
+    })
+    const url = builder.toString()
+    const options = this.defaultOptions()
+
+    const res = BuffettCodeApiClientV3.request(url, options)
+    return res['data']
+  }
+
+  // TODO: latestのサポート
+  public ondemandDaily(ticker: string, date: Date): object {
+    const endpoint = BuffettCodeApiClientV3.baseUrl + '/ondemand/daily'
+    const builder = new UrlBuilder(endpoint, {
+      ticker,
+      date: date.toISOString().substring(0, 10)
+    })
+    const url = builder.toString()
+    const options = this.defaultOptions()
+
+    const res = BuffettCodeApiClientV3.request(url, options)
+    return res['data']
+  }
+}
