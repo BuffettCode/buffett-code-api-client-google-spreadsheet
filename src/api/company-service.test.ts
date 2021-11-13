0b452a3680b3fa4952b64765a93f8422f2868ac8
Akiomi Kamakura <akiomik@gmail.com>
2021-11-13 13:46:12 +0900

    Add CompanyService#isOndemandDailyApiPeriod

--- src/api/company-service.test.ts
+++ src/api/company-service.test.ts
@@ -1,10 +1,15 @@
 import { CompanyService } from '~/api/company-service'
 import { CachingBuffettCodeApiClientV2 } from '~/api/v2/caching-client'
+import { CachingBuffettCodeApiClientV3 } from '~/api/v3/caching-client'
+import { DateParam } from '~/fiscal-periods/date-param'
 import { YearQuarter } from '~/fiscal-periods/year-quarter'
 import { YearQuarterParam } from '~/fiscal-periods/year-quarter-param'
 
 jest.mock('~/api/v2/client', () =>
   jest.requireActual('~/__mocks__/api/v2/client')
+)
+jest.mock('~/api/v3/client', () =>
+  jest.requireActual('~/__mocks__/api/v3/client')
 )
 jest.mock('~/services/company-cache', () =>
   jest.requireActual('~/__mocks__/services/company-cache')
@@ -32,4 +37,23 @@ test('isOndemandQuarterApiPeriod', () => {
   expect(
     service.isOndemandQuarterApiPeriod(new YearQuarterParam('LY', 'LQ'))
   ).toBe(false)
+})
+
+test('isOndemandDailyApiPeriod', () => {
+  const client = new CachingBuffettCodeApiClientV3('token')
+  const service = new CompanyService('2371', client)
+
+  expect(
+    service.isOndemandDailyApiPeriod(new DateParam(new Date('2003-10-10')))
+  ).toBe(true)
+  expect(
+    service.isOndemandDailyApiPeriod(new DateParam(new Date('2016-11-12')))
+  ).toBe(true)
+  expect(
+    service.isOndemandDailyApiPeriod(new DateParam(new Date('2016-11-13')))
+  ).toBe(false)
+  expect(service.isOndemandDailyApiPeriod(new DateParam(new Date()))).toBe(
+    false
+  )
+  expect(service.isOndemandDailyApiPeriod(new DateParam('latest'))).toBe(false)
 })
