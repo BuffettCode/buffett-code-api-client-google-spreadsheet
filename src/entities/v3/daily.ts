7324005c88a378f69f2a999f3f4440ffba3fac88
Akiomi Kamakura <akiomik@gmail.com>
2021-12-08 10:54:52 +0900

    Improve entities

--- src/entities/v3/daily.ts
+++ src/entities/v3/daily.ts
@@ -1,6 +1,38 @@
-export class Daily {
+import { HasColumnDescription, HasPeriod } from '~/entities/v3/interface'
+
+export class Daily implements HasColumnDescription, HasPeriod<Date> {
   constructor(readonly data: object, readonly columnDescription: object) {
     // noop
+  }
+
+  period(): Date {
+    return new Date(this.data['day'])
+  }
+
+  propertyNames(): string[] {
+    return Object.keys(this.columnDescription)
+  }
+
+  labelOf(propertyName: string): string | null {
+    const desc = this.columnDescriptionOf(propertyName)
+    if (desc) {
+      return desc['name_jp']
+    } else {
+      return null
+    }
+  }
+
+  unitOf(propertyName: string): string | null {
+    const desc = this.columnDescriptionOf(propertyName)
+    if (desc) {
+      return desc['unit']
+    } else {
+      return null
+    }
+  }
+
+  private columnDescriptionOf(propertyName: string): string | null {
+    return this.columnDescription[propertyName]
   }
 
   static fromResponse(response: object): Daily {
